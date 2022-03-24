8fe6ae1a21576041f32cc10f2a7a2c4dd1be6042
Ahmet Can Ekinci <ahmet_2003enes@hotmail.com>
2022-03-24 15:02:46 +0300

    Case Studies End

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
-          <div class="carousel-item active">
+          <div class="carousel-item active bg-carousel">
             <div class="mid">
                 <h1 class="header1">Motero Services</h1>
                 <p class="header-p">We design & build brands, campaigns & digital <br> projects for businesses large & small.</p>
@@ -62,21 +62,21 @@
           <div class="box-s">
             <h3>Design <br> Product Software</h3>
             <img src="./assets/images/Illus.png" alt="">
-            <p>We meet customers in set place to discuss the details about needs and demands before proposing a plan.
+            <p class="box-p">We meet customers in set place to discuss the details about needs and demands before proposing a plan.
             </p>
             <a href="">Learn more</a>
           </div>
           <div class="box-s">
             <h3>Design <br> Product Software</h3>
             <img src="./assets/images/Illus1.png" alt="">
-            <p>We meet customers in set place to discuss the details about needs and demands before proposing a plan.
+            <p class="box-p">We meet customers in set place to discuss the details about needs and demands before proposing a plan.
             </p>
             <a href="">Learn more</a>
           </div>
           <div class="box-s">
             <h3>Design <br> Product Software</h3>
             <img src="./assets/images/Illus2.png" alt="">
-            <p>We meet customers in set place to discuss the details about needs and demands before proposing a plan.
+            <p class="box-p">We meet customers in set place to discuss the details about needs and demands before proposing a plan.
             </p>
             <a href="">Learn more</a>
           </div>
@@ -84,7 +84,7 @@
             <div class="special">
             <h3>Design <br> Product Software</h3>
             <img src="./assets/images/Illus.png" alt="">
-            <p>We meet customers in set place to discuss the details about needs and demands before proposing a plan.
+            <p class="box-p">We meet customers in set place to discuss the details about needs and demands before proposing a plan.
             </p>
             <a href="">Learn more</a>
           </div>
@@ -97,16 +97,79 @@
           <div class="img-s"><img src="./assets/images/unsplash_YDVdprpgHv4.png" alt=""></div>
           </div>
           <div class="s-b-right">
-            <div class="text-s">
-              <span>60</span>
+            <div class="text-s margin-s">
+              <div class="text-s-1-1">
+              <span class="text-s-n">60</span>
               <span class="Happly-Client">Happly Client</span>
+              </div>
+              <div class="text-s-1-2">
+               <span class="Lorem-Ipsum-is-simpl">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+              </div>
           </div>
-            <div class="text-s"></div>
-            <div class="text-s"></div>
+            <div class="text-s">
+              <div class="text-s-1-1">
+              <span class="text-s-n">100</span>
+              <span class="Project-Complete">Project Complete</span>
+              </div>
+              <div class="text-s-1-2">
+              <span class="Lorem-Ipsum-is-simpl">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+              </div>
+            </div>
+            <div class="text-s">
+              <div class="text-s-1-1">
+              <span class="text-s-n">355</span>
+              <span class="Happly-Client">Happly Client</span>
+              </div>
+              <div class="text-s-1-2">
+               <span class="Lorem-Ipsum-is-simpl">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <!-- Our Services End-->
+
+    <!-- Case Studies -->
+    <div class="case">
+     <div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators case-carousel">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active bg-carousel-mid">
+      <div class="case-mid">
+        <span class="SATISFIED-CLIENT-STO">SATISFIED CLIENT STORIES</span>
+        <span class="quotes"></span>
+        <span class="Lorem-Ipsum-is-simpl-c">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500</span>
+        <span class="Erin-Green-Ceo-Envat">Erin Green <br><span class="text-style-1">Ceo Envato</span></span>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/images/Mask.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/images/Mask.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+</div>
+    <!-- Case Studies End -->
+    <!-- Elements -->
+      <div class="elements">
+        <h1>Why Chose Us</h1>
+        <div class="e-top">
+         .
+        </div>
+        <div class="e-bottom">
+
+        </div>
+      </div>
+    <!-- Elements End -->
+
+    <!-- Main End -->
   </div>
 </template>
 
